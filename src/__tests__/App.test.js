8d7b6a18beb53acbb4ce8db849d69786ebf507b5
Elumalai Kaliyaperumal <elumalai.kaliyaperumal@gmail.com>
2020-03-06 14:36:50 +0530

    Tried writing test cases for App.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Provider } from 'react-redux'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme'
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+import App from "../components/App"
+import reducer from '../reducers/rootReducer'
+import * as actions from '../actions/index'
+
+configure({ adapter: new Adapter() })
+
+const mockstore = configureMockStore([thunk])
+
+describe('App', () => {
+    it('Renders without crashing', () => {
+        const store = mockstore({ title: 'Hello!' })
+        const wrapper = shallow(
+            <Provider store={store}>
+                <App />
+            </Provider>,
+        )
+        expect(wrapper).toMatchSnapshot()
+    })
+
+    it('Should have a initial state', () => {
+        expect(reducer(undefined, {})).toEqual({ title: 'Hello!' })
+    })
+
+    it('Should render dispatched text', () => {
+        const store = mockstore(reducer)
+        const expectedActions = [{ type: 'TITLE', payload: 'Hello World!' }]
+        store.dispatch(actions.displayTitle('Hello World!'))
+        expect(store.getActions()).toEqual(expectedActions)
+    })
+})
